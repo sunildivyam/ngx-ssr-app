e113b59e35a0e7cc738a07f974392ad5c005cb74
Sunil Kumar <sunil.divyam@gmail.com>
2023-06-24 20:46:51 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {
+  environment
+} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-ssr-app';
+  title = 'ngx-ssr-app' + environment.envConfiguration;
 }
