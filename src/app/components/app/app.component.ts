378099ae0a0ee13fae1038ed4c9db098bf37368c
Sunil Kumar <sunil.divyam@gmail.com>
2024-04-10 11:52:14 +0530

    menu change

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, NgZone } from "@angular/core";
-import { MenuItem } from "@annuadvent/ngx-common-ui/menu";
-import { SpinnerMode } from "@annuadvent/ngx-common-ui/spinner";
-import { ThemeService } from "@annuadvent/ngx-common-ui/theme";
-import { AppSpinnerService } from "../../modules/app-core/services/app-spinner.service";
-import { AppStateService } from "../../modules/app-core/services/app-state.service";
-import { AppState } from "../../modules/app-core/interfaces/app-state.interface";
-import { AppConfigService, AppConfig } from "@annuadvent/ngx-core/app-config";
+import { Component, OnInit, NgZone } from '@angular/core';
+import { MenuItem } from '@annuadvent/ngx-common-ui/menu';
+import { SpinnerMode } from '@annuadvent/ngx-common-ui/spinner';
+import { ThemeService } from '@annuadvent/ngx-common-ui/theme';
+import { AppSpinnerService } from '../../modules/app-core/services/app-spinner.service';
+import { AppStateService } from '../../modules/app-core/services/app-state.service';
+import { AppState } from '../../modules/app-core/interfaces/app-state.interface';
+import { AppConfigService, AppConfig } from '@annuadvent/ngx-core/app-config';
 import {
   SOCIAL_MEDIA_BUTTONS,
-  SocialMediaButton,
-} from "@annuadvent/ngx-common-ui/social-media";
+  SocialMediaButton
+} from '@annuadvent/ngx-common-ui/social-media';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   appConfig: AppConfig;
@@ -22,9 +22,8 @@ export class AppComponent implements OnInit {
   footerNavItems: Array<MenuItem> = [];
   isMainNavOpen: boolean = false;
   SpinnerMode = SpinnerMode;
-  themeFontSizes: Array<string> = ["12px", "16px", "20px"];
+  themeFontSizes: Array<string> = ['12px', '16px', '20px'];
   socialMediaButtons: Array<SocialMediaButton> = [];
-  menuHeight: number = 0;
 
   constructor(
     private themeService: ThemeService,
@@ -62,12 +61,8 @@ export class AppComponent implements OnInit {
     this.socialMediaButtons = SOCIAL_MEDIA_BUTTONS.map((btn) => {
       return {
         ...btn,
-        url: socialMeidaConfig[btn.id],
+        url: socialMeidaConfig[btn.id]
       };
     });
-  }
-
-  public onMainMenuResize(height: number): void {
-    this.zone.run(() => setTimeout(() => (this.menuHeight = height)));
   }
 }
